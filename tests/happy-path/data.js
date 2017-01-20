79db474d8c6c47e6c54d2ad214f890967793866c
lcurryea <lcurry.ea@gmail.com>
2017-01-20 10:47:52 +0000

    Data file update for removal of search buffer

--- tests/happy-path/data.js
+++ tests/happy-path/data.js
@@ -2,6 +2,10 @@ const data = [
   { location: 'L129ES', floodzone: 'flood zone 1' },
   { location: 'WA41HT', floodzone: 'flood zone 2' },
   { location: 'WA41AG', floodzone: 'area that benefits from flood defences' },
-  { location: 'WA51DG', floodzone: 'flood zone 3' }
+  { location: 'WA51BD', floodzone: 'flood zone 3' },
+  { location: 'PO209AE', floodzone: 'flood zone 1' },
+  { location: 'PO200HB', floodzone: 'flood zone 2' },
+  { location: 'IG110HZ', floodzone: 'area that benefits from flood defences' },
+  { location: 'PO200ES', floodzone: 'flood zone 3' }
   ]
 module.exports = data
